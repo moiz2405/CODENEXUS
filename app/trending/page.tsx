20f587502e8c0c34931bce4cb6392321657c9a59
Moiz2405 <km856307@gmail.com>
2024-12-22 23:04:04 +0530

    added trending page

---
+++ app/trending/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Trending from '../../components/courses/Trending'
+function page() {
+  return (
+    <>
+    <div className='w-full px-4 py-8 mx-20'>
+
+    <Trending />
+    </div>
+    </>
+  )
+}
+
+export default page
