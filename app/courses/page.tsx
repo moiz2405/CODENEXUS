e217df78428e177f1e9b5ab2ec658a28dc58151f
Moiz2405 <km856307@gmail.com>
2024-12-25 19:58:01 +0530

    Modified app/courses/page.tsx

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PathCarouselWithCourses from '../../components/courses/PathCarouselWithCourses';
 
 const Page = () => {
-    const paths = ['Full Stack', 'DevOps', 'Web Development','Front End','Back End', 'DSA','Cyber Security', 'Cloud Computing', 'AI/ML']; // Example paths
+    const paths = ['Full Stack', 'Front End','Back End','DSA','DevOps', 'Web Development','Cyber Security', 'Cloud Computing', 'AI/ML']; // Example paths
 
     return (
         <div>
